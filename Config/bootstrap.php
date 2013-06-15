bd4b2947ed9ff831848f2e68e33481ee190b92b7
Rob Sawyer <robksawyer@gmail.com>
2013-06-14 18:28:41 -0700

    Minor tweaks

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -4,9 +4,15 @@
 */
 	$amazon = array(
 		'Aws' => array(
+			'url' => getenv('CC_S3_URL'),
 			'key' => getenv('CC_S3_ACCESS_KEY'),
 			'secret' => getenv('CC_S3_SECRET_KEY')
 		)
 	);
 	Configure::write('Aws', $amazon);
+	$amazon_config_data = Configure::read('Aws');
+	define('CC_S3_URL', $amazon_config_data['url']);
+	define('CC_S3_ACCESS_KEY', $amazon_config_data['key']);
+	define('CC_S3_SECRET_KEY', $amazon_config_data['secret']);
+	debug(CC_S3_SECRET_KEY);
 ?>
